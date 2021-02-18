ab4c36709fe32495c7d0ee09158bd74f8584de83
t.terai <tomoya.terai01@gmail.com>
2021-02-18 14:14:00 +0900

    heroku schedulerのテスト

--- tweet.rb
+++ tweet.rb
@@ -2,6 +2,9 @@ require 'date'
 require 'twitter'
 require 'dotenv/load'
 
+# require "./src/Weather"
+# require "./src/WeatherInfo"
+
 class Tweet  
   def initialize
     # 投稿内容の初期化
@@ -23,8 +26,16 @@ class Tweet
 
   # ツイート本文の生成
   def create_text
-    # 投稿内容を設定
-    @text = "test"
+    @text="test2"
+    # 天気情報取得
+    # weatherobj = Weather.new
+    # info = weatherobj.doProcess()
+
+    # # ツイート部分
+    # @tweet = "今日、#{info.today()} 東京の天気です\n"
+    # @tweet += "天気は#{info.todayTelop()}\n"
+    # @tweet += "最高気温は【#{info.todayTempMax()}℃】\n"
+    # @tweet += "最低気温は【#{info.todayTempMin()}℃】\n"
   end
 
   private
