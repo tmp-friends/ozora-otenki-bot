e42d1a65a59f5bb50766a614ae4106a3dc01bfc2
t.terai <tomoya.terai01@gmail.com>
2021-02-19 17:38:43 +0900

    envファイルの設定

--- tweet.rb
+++ tweet.rb
@@ -11,10 +11,10 @@ class Tweet
     @text = ""
     # クライアントの生成
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "O4BM7HR45NQQrQ632Nsd4vRDB"
-      config.consumer_secret     = "Yxzlxr1MYsLWsJGWEcoEVXwuO5aJr79i9q2MiAJPktDkubanIP"
-      config.access_token        = "1342345802253885441-NfGtwXeUhjaZdRFEvYFoRL6eduJO5l"
-      config.access_token_secret = "wnsVKDWmHHMuUnbd9pX9YRPwKnhCjMaNuOngqvieKFvyw"
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
     end
   end
 
@@ -31,10 +31,10 @@ class Tweet
     info = weatherobj.doProcess()
 
     # ツイート部分
-    @text = "みなさんおはようございます！ 時刻は7時30分！\n"
+    @text = "みなさんおはようございます！\n時刻は7時30分！\n"
     @text += "今日のお空はどんな空～❓\n大空お天気の時間です✨\n"
     @text += "今日の都心部は#{info.todayTelop()}、最高気温は#{info.todayTempMax()}℃です！\n"
-    @text += "それでは通勤・通学気をつけて，行ってらっしゃ～い！"
+    @text += "それでは通勤・通学気をつけて、行ってらっしゃ～い！"
   end
 
   private
@@ -44,7 +44,7 @@ class Tweet
     begin
       images = []
 
-      range = 0..4
+      range = 0..22
       range.each{|i|    
         images << File.new("./images/otenki#{i}.jpg")
       }
